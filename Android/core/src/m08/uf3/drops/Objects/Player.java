6478cae0eec6abc5ab747e97ff05437be4a71f25
a20alvlozhue <a20alvlozhue@inspedralbes.cat>
2023-02-07 12:39:09 +0100

    clase bullet

--- Android/core/src/m08/uf3/drops/Objects/Player.java
+++ Android/core/src/m08/uf3/drops/Objects/Player.java
@@ -205,32 +205,6 @@ public class Player extends Actor {
             getParent().addActor(bullet);
         }
     }
-    public class Bullet extends Actor {
-        private Texture texture;
-        private Vector2 position;
-        private float xVelocity;
-        private float yVelocity;
-
-        public Bullet(float x, float y, float xVelocity, float yVelocity, Texture texture) {
-            this.position = new Vector2(x, y);
-            this.xVelocity = xVelocity;
-            this.yVelocity = yVelocity;
-            this.texture = texture;
-        }
-
-        @Override
-        public void act(float delta) {
-            super.act(delta);
-            position.x += xVelocity * delta;
-            position.y += yVelocity * delta;
-        }
-
-        @Override
-        public void draw(Batch batch, float parentAlpha) {
-            super.draw(batch, parentAlpha);
-            batch.draw(texture, position.x, position.y);
-        }
-    }
 
 
     private TextureRegion getPLayerDirection() {
